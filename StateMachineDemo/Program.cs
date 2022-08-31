b14007d5cabd0ce549009a179edc94b6bed4dc88
Archie Fan <archiefan@gmail.com>
2022-08-31 18:52:01 +0100

    Add project files.

---
+++ StateMachineDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Stateless;
+using Stateless.Graph;
+
+namespace StateMachine
+{
+
+
+    class Program
+    {
+        enum Input
+        {
+            Coin,
+            Push
+        }
+        enum State
+        {
+            Locked,
+            Unlocked
+        }
+        static void Main()
+        {
+            var stateMachine = new StateMachine<State, Input>(State.Locked);
+            stateMachine.Configure(State.Locked)
+                .Permit(Input.Coin, State.Unlocked)
+                .PermitReentry(Input.Push);
+            stateMachine.Configure(State.Unlocked)
+              .Permit(Input.Push, State.Locked)
+              .PermitReentry(Input.Coin);
+            stateMachine.Fire(Input.Coin);
+            Console.WriteLine(stateMachine.State);
+
+            var graph = UmlDotGraph.Format(stateMachine.GetInfo());
+            Console.WriteLine(graph);
+
+            Console.ReadLine();
+        }
+    }
+}
+
